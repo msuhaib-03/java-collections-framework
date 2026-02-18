4024502667b813947ce61f376638e3e9bea1730f
adrenalinejunkie03 <muhammadsuhaib2805@gmail.com>
2026-02-18 19:55:57 +0500

    Removal of elements, replacing and checking if present

--- src/List_Interface/Array_List.java
+++ src/List_Interface/Array_List.java
@@ -26,7 +26,22 @@ public class Array_List {
         for(Integer i: list){
             System.out.println(i);
         }
+        // i is not a pointer but an element itself here and "list"; a variable over which you want to run the loop on.
 
+        // Statements to check if the list contains a specific element
+        System.out.println(list.contains(6)); // returns false as 6 is not present in the list
+        System.out.println(list.contains(5)); // returns true as 5 is present in the
+
+        list.remove(0); // removes the element at index 0 (which is 1 in this case)
+
+        // It adds the specified element at the specified index in the list. In this case, it adds the element 10 at index 1.
+        list.add(1,10);
+        System.out.println(list);
+        // currently list has 2 elements, 5 and 10.
+
+        // Now we can even replace elements.
+        list.set(0,22); // replaces the element at index 0 (which is 5) with 22
+        System.out.println(list);
     }
 
 }

9d5cd8b91dea1567aa9539074e55822fafe159e0
adrenalinejunkie03 <muhammadsuhaib2805@gmail.com>
2026-02-22 23:38:11 +0500

    LinkedList and its methods as Stack

--- src/List_Interface/Stack_Interface.java
+++ src/List_Interface/Stack_Interface.java
@@ -1,5 +1,6 @@
 package List_Interface;
 
+import java.util.LinkedList;
 import java.util.Stack;
 
 public class Stack_Interface {
@@ -30,6 +31,17 @@ public class Stack_Interface {
         int search = stack.search(5); // returns the 1-based position of the element from the top of the stack, which is 2 in this case as 1 is the second element from the top.
         System.out.println("Position of element 5 from top of stack: " + search); // Returns 1.
 
+    // Stack is used either by implementing Stack class or LinkedList class as a stack. Stack class is synchronized and thread-safe but LinkedList is not synchronized and not thread-safe. So if you want to use stack in multi-threaded environment, you should use Stack class. But if you want to use stack in single-threaded environment, you can use either Stack class or LinkedList class as a stack.
+        LinkedList<Integer> stack2 = new LinkedList<>();
+        stack2.addLast(8); //peek
+        stack2.addLast(3); // it is bottom element now.
+        stack2.addLast(13);
+        stack2.addLast(9); // it is top element now.
+        System.out.println("Stack2: " + stack2);
 
+        System.out.println("Stack2 Last element:"+stack2.getLast()); // peek
+        stack2.removeLast(); // pop
+
+        System.out.println("Stack2 after pop: " + stack2);
     }
 }

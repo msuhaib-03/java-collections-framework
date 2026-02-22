3549b6559913ddf16030eb2fb5b9cf37716f8476
adrenalinejunkie03 <muhammadsuhaib2805@gmail.com>
2026-02-22 07:47:48 +0500

    LinkedList in java and using pre-defined methods

---
+++ src/List_Interface/Linked_List.java
@@ -0,0 +1,24 @@
+package List_Interface;
+
+import java.util.LinkedList;
+
+public class Linked_List {
+    public static void main(String[] args) {
+            // LinkedList is a data structure that consists of a sequence of nodes, where each node contains data and a reference (or link) to the next node in the sequence. It is a dynamic data structure that allows for efficient insertion and deletion of elements.
+
+            // In a LinkedList, each node typically contains two parts: the data (which can be of any type) and a reference to the next node in the list. The first node is called the head, and the last node is called the tail. The tail's next reference is usually set to null to indicate the end of the list.
+
+            // LinkedLists are often used when you need to frequently add or remove elements from the list, as they can be more efficient than arrays for these operations. However, they may require more memory than arrays due to the additional references stored in each node.
+
+            // Java provides a built-in LinkedList class in the java.util package, which implements the List interface and provides various methods for manipulating linked lists.
+        LinkedList<Integer> linkedList = new LinkedList<>();
+        linkedList.add(1);
+        linkedList.add(5);
+        System.out.println(linkedList);
+        System.out.println("Specific index element of a list: "+linkedList.get(0)); // this operation perfom in O(n) time complexity as we have to traverse the list to reach the specific index.
+
+        linkedList.addFirst(7); // adds 7 at the beginning of the list and this operation performs in O(1) time complexity as we just have to update the head reference to point to the new node and the new node's next reference to point to the old head.
+        linkedList.addLast(0); // adds 9 at the end of the list and 0(1) too as we just have to update the tail reference to point to the new node and the new node's next reference to null.
+        System.out.println(linkedList);
+    }
+}
